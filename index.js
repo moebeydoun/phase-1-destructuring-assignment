dab5f1a80025819a52d1e9f3deb6d5340d630f25
moe beydoun <moebeydoun24@yahoo.com>
2023-11-17 18:11:35 -0500

    complete lab

--- index.js
+++ index.js
@@ -46,3 +46,34 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+// Strings
+
+
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+
+
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+
+
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
+
+
+
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+
+
+const [r, o, y, g, b, , v] = colors;
+
+
+const [indg] = colors.slice(5, 6);
+
+// Objects
+
+
+const { muppetName, color, song, job, partner } = muppet;
+
+// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const { nestedJob, nestedPartner } = nestedMuppet;
+const { song2, song4 } = nestedMuppet.album.theMuppetMovie;
